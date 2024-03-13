0abce81cc31b379927871268abea529e62db2f35
Pavel.WSL <pas-zhukov@yandex.ru>
2024-03-13 13:30:31 +0300

    обновил скрипты

--- install_script.sh
+++ install_script.sh
@@ -2,9 +2,12 @@
 
 
 INSTALL_PATH="/opt/polytech-orchestra-bot/"
-SERVICE_PATH="/etc/systemd/system/orchestra-bot-schedule.service"
-TIMER_PATH="/etc/systemd/system/orchestra-bot-schedule.timer"
-TARGET="whitehall_checker_bot.py"
+
+#Check user rights
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
 
 # shellcheck disable=SC2164
 cd ${INSTALL_PATH}
@@ -14,55 +17,11 @@ apt install python3-virtualenv
 virtualenv venv
 ./venv/bin/pip install -r requirements.txt
 
-#Check user rights
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root"
-    exit 1
-fi
+# Make scripts runnable
+chmod +x ./birthday_notificator_install.sh
+chmod +x ./whitehall_checker_install.sh
 
-#Remove old version
-if [[ -d $SERVICE_PATH ]]; then
-    echo "---"
-    # rm -rfv $INSTALL_PATH
-    # rm  -v $CONFIGURATION
-    rm -v $SERVICE_PATH
-    rm -v $TIMER_PATH
-fi
-
-# Creating service in systemd
-
-touch $SERVICE_PATH
-# Fill .service file
-exec 1> $SERVICE_PATH
-echo "[Unit]"
-echo "Description=Orchestra one-time schedule check bot"
-echo ""
-echo "[Service]"
-echo "Type=oneshot"
-echo "ExecStart=${INSTALL_PATH}/venv/bin/python3 ${INSTALL_PATH}${TARGET}"
-echo ""
-echo "User=root"
-echo "Group=root"
-echo "[Install]"
-echo "WantedBy=multi-user.target"
-
-touch $TIMER_PATH
-# Fill .timer file
-exec 1> $TIMER_PATH
-echo "[Unit]"
-echo "Description=Orchestra one-time schedule check bot"
-echo ""
-echo "[Timer]"
-echo "OnCalendar=Mon,Fri *-09..12,01..05-* 15:*:*"
-echo ""
-echo "[Install]"
-echo "WantedBy=timers.target"
-
-
-
-systemctl daemon-reload
-systemctl enable orchestra-bot-schedule.service
-systemctl enable orchestra-bot-schedule.timer
-systemctl start orchestra-bot-schedule.timer
+./birthday_notificator_install.sh
+./whitehall_checker_install.sh
 
 
---
+++ whitehall_checker_install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+SERVICE_NAME="orchestra-bot-schedule"
+INSTALL_PATH="/opt/polytech-orchestra-bot/"
+SERVICE_PATH="/etc/systemd/system/${SERVICE_NAME}.service"
+TIMER_PATH="/etc/systemd/system/${SERVICE_NAME}.timer"
+TARGET="whitehall_checker_bot.py"
+
+# Remove old version
+if [[ -d $SERVICE_PATH ]]; then
+    echo "---"
+    rm -v $SERVICE_PATH
+    rm -v $TIMER_PATH
+fi
+
+# Creating service in systemd
+
+touch $SERVICE_PATH
+# Fill .service file
+exec 1> $SERVICE_PATH
+echo "[Unit]"
+echo "Description=Orchestra one-time schedule check bot"
+echo ""
+echo "[Service]"
+echo "Type=oneshot"
+echo "ExecStart=${INSTALL_PATH}/venv/bin/python3 ${INSTALL_PATH}${TARGET}"
+echo ""
+echo "User=root"
+echo "Group=root"
+echo "[Install]"
+echo "WantedBy=multi-user.target"
+
+touch $TIMER_PATH
+# Fill .timer file
+exec 1> $TIMER_PATH
+echo "[Unit]"
+echo "Description=Orchestra one-time schedule check bot"
+echo ""
+echo "[Timer]"
+echo "OnCalendar=Mon,Fri *-09..12,01..05-* 15:*:*"
+echo ""
+echo "[Install]"
+echo "WantedBy=timers.target"
+
+
+systemctl daemon-reload
+systemctl enable ${SERVICE_NAME}.service
+systemctl enable ${SERVICE_NAME}.timer
+systemctl start ${SERVICE_NAME}.timer
