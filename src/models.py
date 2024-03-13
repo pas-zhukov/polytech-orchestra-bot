09001b743a35d1fe05b16bcd95703883bfc0e386
Pavel.WSL <pas-zhukov@yandex.ru>
2024-03-13 11:23:06 +0300

    add user model

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
 class Concerto:
@@ -8,3 +8,17 @@ class Concerto:
 
     def get_stringed_time(self):
         return self.datetime.strftime("%H:%M")
+
+
+class User:
+    def __init__(self, name: str, birthdate: date, instrument: str,
+                 tg: str, vk: str, is_active: bool,
+                 activity_start: date, activity_end: date) -> None:
+        self.name: str = name
+        self.birthdate: date = birthdate
+        self.instrument: str = instrument
+        self.tg: str = tg
+        self.vk: str = vk
+        self.is_active: bool = is_active
+        self.activity_start: date = activity_start
+        self.activity_end: date = activity_end
